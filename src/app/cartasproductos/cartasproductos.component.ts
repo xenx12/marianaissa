d256943c379943a0462adbc04a3c00d076d10ec0
xenx12 <origamilombanoj@gmail.com>
2022-09-05 04:12:52 -0300

    se añadio el archivo json

--- src/app/cartasproductos/cartasproductos.component.ts
+++ src/app/cartasproductos/cartasproductos.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import productosJson from './productos.json';  
+
+interface Producto {  
+  id: Number;  
+  titulo: String;  
+  img: String;  
+  textocorto: String;  
+  caracteristicas: String;  
+  precio: String;  
+}  
 
 @Component({
   selector: 'app-cartasproductos',
@@ -6,10 +16,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cartasproductos.component.css']
 })
 export class CartasproductosComponent implements OnInit {
+  
+  productos: Producto[] = productosJson;  
 
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
